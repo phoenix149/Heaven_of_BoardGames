2b4e1dc57593a3d0e9a80ecd12cc599711d362e1
AKABI NANGA André <andreparfaitakabinanga@gmail.com>
2025-01-21 12:51:36 +0000

    Modification Page Connexion Mar21Jan2025

--- connexion.php
+++ connexion.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -10,20 +12,84 @@
 </head>
 
 <body>
-<?php include './includes/header.php'?>
+
+<?php
+session_start(); 
+include './includes/header.php'
+?>
+<?php
+// Configuration de la base de données
+$host = 'localhost';
+$dbname = 'fil_rouge';
+$username = 'root';
+$password = '';
+
+try {
+    // Connexion à la base de données
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
+
+
     <main>
 
+    <?php  
+// Vérification si l'utilisateur est déjà connecté
+if (isset($_SESSION['user_id'])) {
+	header('Location: index.php'); // Redirection vers la page d'accueil si l'utilisateur est déjà connecté
+	exit();
+}
+
+// Traitement de la soumission du formulaire de connexion
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	// Récupération des données du formulaire en méthode POST
+	$login = $_POST['login'];
+	$password = $_POST['password'];
+
+
+	$stmt = $pdo->prepare("SELECT * FROM utilisateurs WHERE u_pseudo=u_pseudo;");
+	$stmt->bindValue(':login', $login);
+	$stmt->execute();
+	$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+	if ($user && isset($user['tu_id']) && password_verify($password, $user['Password'])) {
+        // Connexion réussie, stockage de l'identifiant de l'utilisateur dans la variable de session
+        $_SESSION['user_id'] = $user['Id_User'];
+    
+        //recup le type d'utilisateur pour renseigner la variable de session user_type
+        $stmt = $pdo->prepare("SELECT * FROM t_d_usertype WHERE Id_UserType=:typeuser");
+        $stmt->bindValue(':typeuser', $user['Id_UserType']);
+        $stmt->execute();
+        $usert = $stmt->fetch(PDO::FETCH_ASSOC);
+        $_SESSION['user_type'] = $usert['Libelle'];
+        $_SESSION['logged_in'] = true;
+        header('Location: index.php'); // Redirection vers la page d'accueil
+        exit();
+    } else {
+        // Identifiants incorrects, affichage d'un message d'erreur
+        $error_message = "Email ou mot de passe incorrect.";
+    }
+    
+}
+?>
+
     <h1>Bienvenue sur Heaven of BoardGames</h1>
+    <?php if (isset($error_message)) : ?>
+	<p><?php echo $error_message; ?></p>
+<?php endif; ?>
     <p>Veuillez vous connectez pour voir les jeux</p>
     <p>Pas de compte ? <a href="inscription.php">S'inscrire</a></p>
         <form action="#" method="post">
             <div>
-                <label for="">Identifiant</label>
-                <input type="text" name="identifiant" require>
+                <label for="login">Identifiant</label>
+                <input type="text" name="login" required>
             </div>
             <div>
-                <label for="">Mot de passe</label>
-                <input type="text" name="mdp" require>
+                <label for="password">Mot de passe</label>
+                <input type="password" name="password" required>
             </div>
             <div>
                 <input type="submit" name="connexion" value="Connexion">
