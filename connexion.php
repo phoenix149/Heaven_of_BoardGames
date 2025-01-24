a20b6998e6a89a53ac1c252c6e22836ab26aaf2d
AKABI NANGA André <andreparfaitakabinanga@gmail.com>
2025-01-24 14:31:05 +0100

    Heaven_of_BoardGames-Andre3_ModifConnexionOK

--- connexion.php
+++ connexion.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Heaven Of BoardGames</title>
+</head>
+<body>
 
 <?php include './includes/header.php';?>
 
@@ -11,7 +22,7 @@ if (isset($_SESSION['u_id'])) {
 }
 
 // Traitement de la soumission du formulaire de connexion
-if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Récupération des données du formulaire en méthode POST
     $login = htmlspecialchars(trim($_POST['login']));
     $password = htmlentities(trim($_POST['password']));
@@ -25,11 +36,11 @@ if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') 
 
     if ($user && password_verify($password, $user['u_mdp'])) {
         // Connexion réussie, stockage de l'identifiant de l'utilisateur dans la variable de session
-        $_SESSION['u_id'] = $user['u_id'];
+        $_SESSION['user_id'] = $user['u_id'];
 
         //recup le type d'utilisateur pour renseigner la variable de session user_type
-        $stmt = $pdo->prepare("SELECT * FROM type_utilisateur WHERE tu_id=tu_id");
-        $stmt->bindValue(':tu_id', $user['tu_id']);
+        $stmt = $pdo->prepare("SELECT * FROM type_utilisateur WHERE tu_id=:typeuser");
+        $stmt->bindValue(':typeuser', $user['tu_id']);
         $stmt->execute();
         $usert = $stmt->fetch(PDO::FETCH_ASSOC);
         $_SESSION['user_type'] = $usert['tu_libelle'];
@@ -69,4 +80,5 @@ if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') 
     </main>
 
     <?php include './includes/footer.php'; ?>
- 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
