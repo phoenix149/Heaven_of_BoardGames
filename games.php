3754027954056d35280767542423d3ee72dd5ae5
AKABI NANGA André <andreparfaitakabinanga@gmail.com>
2025-01-30 15:07:11 +0000

    correction des bugs

--- games.php
+++ games.php
@@ -1,4 +1,4 @@
- <?php include './includes/header.php' ?>
+<?php include './includes/header.php' ?>
     <main>
 
 
@@ -204,7 +204,7 @@
             </div>
         </div>
     </div>
-
-    <script src="script/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="/script/script.js"></script>
     </main>
     <?php include './includes/footer.php' ?>
