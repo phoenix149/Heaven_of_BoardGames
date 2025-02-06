db82aae6fac034d428f4b6cdd018689637c9f03f
Baïdy GALLO <gallo.baidy@gmail.com>
2025-02-06 09:18:13 +0100

    Modif de la bdd

--- doc/bdd_fil_rouge.sql
+++ doc/bdd_fil_rouge.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 31 jan. 2025 à 08:43
+-- Généré le : jeu. 06 fév. 2025 à 09:16
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -95,21 +95,6 @@ CREATE TABLE `categories` (
   `ctg_nom` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `categories`
---
-
-INSERT INTO `categories` (`ctg_id`, `ctg_nom`) VALUES
-(1, 'Jeu de plateau'),
-(2, 'Jeu de dés'),
-(3, 'Jeu de cartes'),
-(4, 'Jeu de coopération'),
-(5, 'Jeu de lettres'),
-(6, 'Jeu d\'adresse'),
-(7, 'Jeu de connaissances'),
-(8, 'Jeu de logique'),
-(9, 'Jeu d\'enquête');
-
 -- --------------------------------------------------------
 
 --
@@ -178,13 +163,13 @@ INSERT INTO `editeur` (`edit_id`, `edit_nom`) VALUES
 
 CREATE TABLE `jeu` (
   `jeu_id` int(11) NOT NULL,
-  `jeu_nom` varchar(50) NOT NULL,
+  `jeu_nom` varchar(100) NOT NULL,
   `jeu_img` varchar(500) NOT NULL,
   `jeu_prix` decimal(15,2) NOT NULL,
   `jeu_EAN` varchar(50) NOT NULL,
   `jeu_dte_creation` int(4) NOT NULL,
   `jeu_nb_joueurs` varchar(50) NOT NULL,
-  `jeu_description` varchar(2500) NOT NULL,
+  `jeu_description` varchar(2500) DEFAULT NULL,
   `jeu_temps` varchar(50) NOT NULL,
   `jeu_qte_stc` int(11) NOT NULL,
   `jeu_note` decimal(2,1) DEFAULT NULL,
@@ -194,23 +179,6 @@ CREATE TABLE `jeu` (
   `age_id` int(11) NOT NULL,
   `m_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `jeu`
---
-
-INSERT INTO `jeu` (`jeu_id`, `jeu_nom`, `jeu_img`, `jeu_prix`, `jeu_EAN`, `jeu_dte_creation`, `jeu_nb_joueurs`, `jeu_description`, `jeu_temps`, `jeu_qte_stc`, `jeu_note`, `edit_id`, `pays_id`, `ctg_id`, `age_id`, `m_id`) VALUES
-(1, 'La Guerre de l\'Anneau : Le Jeu de Cartes', 'https://les-meeples.fr/images/boardgames/main/la-guerre-de-lanneau-le-jeu-de-cartes-contre-lombre-extension_1719915103.jpg', 18.80, '2199000145717', 2004, '2 à 4 joueurs', 'Deux à quatre joueurs s’affrontent en deux équipes, les Peuples Libres contre l’Ombre. Les Peuples Libres tentent désespérément de mener à bien la destruction de l’Anneau. L’Ombre doit frapper rapidement et de manière décisive avant que les porteurs de l’Anneau ne réussissent leur quête\r\n\r\nChaque joueur possède des cartes représentant les forces et les faiblesses des factions impliquées dans la guerre. Pour gagner, il va falloir se focaliser sur votre objectif mais aussi surveiller les actions de vos adversaires. Pendant une manche, vous jouez des cartes représentant des personnages, armées, objets et événements de la Guerre de l’Anneau. À la fin de chaque manche, les combats sont résolus. Vous gagnez des points de victoire en remportant des combats. Plusieurs scénarios possibles, (de la Comté à la Montagne du Destin, un autre basé sur le premier livre, ou encore le duel, pour deux joueurs).', '1 à 2h', 12, NULL, 2, 2, 1, 2, 4),
-(2, 'Les Loups-Garous de Thiercelieux', 'https://m.media-amazon.com/images/I/81ok3GQCS4L.jpg', 9.95, '3558380086000', 1986, '8 à 24 joueurs', 'De nouvelles options de jeu, plus de pouvoirs révélés pour les joueurs, une compatibilité parfaite avec l’extension Nouvelle Lune, plus de 120 combinaisons possibles entre personnages et métiers… on s’en lèche les babines !\r\nAttribuez des logements et un métier aux Villageois, regardez-les se disputer ces fonctions et s’éliminer les uns les autres pour les obtenir… Une profondeur de jeu inégalée et des centaines d’heures de jeu en perspective !\r\nCette extension nécessite le jeu de base pour être jouée.', '30 minutes à 1h', 7, NULL, 7, 1, 3, 3, 2),
-(3, 'Uno', 'https://www.didacto.com/10860-large_default/uno.jpg', 13.95, '746775036744', 1971, '2 à 10 joueurs', 'Uno est sans aucun doute le jeu de cartes le plus célèbre du monde. \r\n\r\nCréé aux États-Unis en 1971 mais édité seulement en 1985, ce jeu de société s\'appuie sur les règles de base du 8 américains, agrémenté de quelques règles supplémentaires qui le rendent unique. Uno est un jeu de défausse et de réflexe qui repose sur la rapidité et la prise de décision. \r\n\r\nAvec des règles faciles à apprendre, vous serez rapidement gagné par la frénésie du jeu. Uno est un excellent jeu d\'ambiance pour 2 à 10 joueurs.', 'Moins de 30 minutes', 8, NULL, 4, 1, 3, 1, 1),
-(4, 'Harry Potter - Hogwarts Battle', 'https://m.media-amazon.com/images/I/91ImqKtxF3L._AC_UF1000,1000_QL80_.jpg', 63.50, '700304047700', 2016, '2 à 4 joueurs', 'C\'est à quatre élèves d\'assurer la sécurité de l\'école en vainquant les méchants et en consolidant leurs défenses. Dans le jeu, les joueurs jouent le rôle d\'un étudiant de Poudlard : Harry, Ron, Hermione ou Neville, chacun avec son propre jeu de cartes qui est utilisé pour acquérir des ressources.\r\n\r\nEn gagnant de l\'influence, les joueurs ajoutent plus de cartes à leur jeu sous la forme de personnages iconiques (Hagrid, Severus Rogue, Sirius Black, Dumbledore...), de sorts et d\'objets magiques. D\'autres cartes leur permettent de recouvrer leurs points de santé ou de lutter contre Voldemort et ses alliés (Bellatrix Strange, Dolores Ombrage, Draco Malefoy et bien d\'autres encore), les empêchant ainsi de gagner du pouvoir. Les méchants contrecarrent les joueurs avec leurs attaques et l\'art de la magie noire. Ce n\'est qu\'en travaillant ensemble que les joueurs pourront vaincre tous les méchants et protéger le château des forces du mal.', '30 minutes à 1h', 10, NULL, 2, 2, 1, 4, 4),
-(5, 'Champ D\'Honneur', 'https://m.media-amazon.com/images/I/71Y3mSegReL.jpg', 45.00, '3421272118717', 1987, '2 à 4 joueurs', 'Champ D\'Honneur est un wargame avec une mécanique de bag-building. \r\nAu début du jeu, recrutez vos armées en sélectionnant par un système de draft les unités que vous utilisez ensuite pour capturer les points clés sur le plateau. Pour réussir dans Champ D\'Honneur, vous devez gérer avec succès non seulement vos armées sur le champ de bataille, mais aussi celles qui attendent d\'être déployées.\r\nÀ chaque tour, vous tirez trois pièces de votre sac, puis vous les utilisez à tour de rôle pour effectuer des actions. Chaque pièce montre une unité militaire sur une face et peut être utilisée pour une ou plusieurs actions. ', '30 minutes à 1h', 2, NULL, 5, 3, 7, 4, 5),
-(6, 'Dice Throne - Adventures - Occasion', 'https://www.okkazeo.com/images/jeux/38856.png', 67.43, '2199000145823', 2020, '1 à 4 joueurs', 'De l\’exploration, de l\’or, des trésors, des améliorations, des sbires, des combats de boss, et même une marchande nommée Rosella !\r\n\r\nOeuvrez ensemble pendant quelques nuits pour voyager des Sables Pourpres jusqu’à la salle du trône du Roi Fou. Vos héros gagneront des améliorations de deck permanentes à chaque session, mais ne soyez pas trop confiant… Le Roi n’a pas perdu une seule bataille en mille ans et il est impatient de relever un vrai défi !', '1 à 2h', 16, NULL, 7, 1, 7, 4, 3),
-(7, 'Roll Player Adventures', 'https://m.media-amazon.com/images/I/71ZK0mtxc+L.jpg', 90.80, '2199000145984', 2023, '1 à 4 joueurs', 'Les personnages du joueur font face à des défis, explorent de nouvelles terres, se font des amis et des ennemis, résolvent des énigmes, combattent des monstres et prennent des décisions importantes qui façonneront l\'histoire au fur et à mesure qu\'ils progressent dans 11 aventures principales et une quête secondaire jouable.\r\nChoisissez l\'un des nombreux personnages, ou importez un personnage favori de Roll Player et emmenez-le dans un voyage héroïque. Si vous importez un personnage que vous avez créé dans Roll Player, toutes les extensions et toutes les cartes promotionnelles utilisées peuvent continuer à être utilisées dans les aventures de Roll Player.', '1 à 2h', 5, NULL, 3, 3, 6, 4, 6),
-(8, 'The Great Wall', 'https://paradoxetemporel.fr/wp-content/uploads/2022/05/awrgw01fr_box3d_20220124.jpg', 63.92, '2199000145939', 2000, '1 à 4 joueurs', 'La Grande Muraille se dressait déjà pendant la dynastie Zhou, bien des années avant les troubles actuels. À l’époque, il s’agissait d’une simple série de murs et de forts qui protégeait le pays contre les invasions de tribus nomades. La muraille a survécu à de nombreuses guerres et batailles et été agrandie, reconstruite et réparée à maintes reprises… Bien plus tard, durant les Xe et XIe siècles, pour se défendre des invasions Jurchen Jin, la dynastie des Song du Nord a construit les sections de la Grande Muraille, localisées dans ce qui constitue aujourd’hui les provinces de Shanxi et de Hebei. Malgré le travail des Song, le mur n’a pas tenu, les forçant à se retirer au sud. Les formidables fortifications appartenaient maintenant à leurs ennemis.\r\nCent ans plus tard. Se servant de la Grande Muraille érigée par ses prédécesseurs, la dynastie Jin a tenté de repousser l’invasion de Mongols venus du nord. Mais la dynastie Song, à présent nommée Song du Sud, conserve ses rancunes envers les Jin. Les Song se sont alliés avec les Mongols pour écraser leur ancien ennemi. Ils n’ont cependant pas prévu la soif insatiable de leurs « alliés », et doivent maintenant faire face à leur tour à la horde mongole. C’est ici que commence notre histoire…', '2 à 3 heures', 8, NULL, 5, 1, 9, 3, 4),
-(9, 'Lewis & Clark', 'https://cdn3.philibertnet.com/475376-thickbox_default/lewis-clark.jpg', 32.72, '2199000145885', 2013, '1 à 5 joueurs', 'Dans Lewis & Clark, chaque joueur conduit son propre \'Corps of Discovery\' à travers le continent. Son équipe sera complétée par les Indiens et les trappeurs rencontrés au long du voyage.Les joueurs conduisent une expédition à travers le continent Nord-Américain. Votre but ? Atteindre la côte Pacifique le premier en traversant les États-Unis. Lewis & Clark est donc un jeu de course. \r\n\r\nChaque joueur conduit sa propre expédition à travers les États-Unis. Chaque joueur a en main des cartes Personnage qui forment son expédition. Ces personnages permettent d’effectuer des actions telles que récolter des ressources, avancer sur le parcours ou demander de l’aide aux Indiens. Pour être déclenchée, l’action correspondant à une carte doit être associée à une Force fournie soit par une autre carte soit par des Indiens. À chaque tour de jeu, un joueur doit réaliser une action. Il peut aussi recruter un nouveau Personnage et améliorer sa main de cartes. Enfin, régulièrement, les joueurs pourront établir leur Camp afin de reprendre en main les cartes déjà jouées. Mais le chargement de ressources et le nombre d’Indiens accueillis peuvent éventuellement faire perdre l’avance durement gagnée. La gestion des ressources et des Indiens est donc cruciale dans le jeu. La partie s’arrête immédiatement dès qu’un joueur a amené son Camp sur ou au delà de Fort Clatsop.\r\n\r\nSeule la première place compte.', '1 à 2 heures', 11, NULL, 5, 3, 2, 4, 6),
-(10, 'Dragomino', 'https://cdn.cultura.com/cdn-cgi/image/width=830/media/pim/68_366284_1_10_FR.PNG', 13.97, '2199000145182', 2010, '2 à 4 joueurs', 'Vous avez été nommé «dresseur de dragon» et vous avez la chance de partir à leur rencontre sur une île mystérieuse. Mais vous n’êtes pas le seul dresseur envoyé sur ces terres. Qui de vous découvrira le plus de bébés dragons ? \r\n\r\nDragomino est une version enfant de Kingdomino à jouer à partir de 5 ans, dans laquelle vous retrouverez les bonnes sensations de Kingdomino!\r\n\r\nPartez explorer l’île aux dragons et tentez de découvrir un maximum de bébés dragons dans les différents paysages que vous traversez !', 'Moins de 30 minutes', 12, NULL, 6, 3, 2, 1, 3),
-(11, '12 Chip Trick', 'images/679c7a3424aa6_12-chip-trick.webp', 16.95, '2199000146325', 2002, '2 à 4 Joueurs', 'voyons voir\r\n', 'Moins de 30 minutes', 2, 6.8, 4, 2, 5, 3, 2);
 
 -- --------------------------------------------------------
 
@@ -222,23 +190,6 @@ CREATE TABLE `jeu_auteurs` (
   `jeu_id` int(11) NOT NULL,
   `a_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `jeu_auteurs`
---
-
-INSERT INTO `jeu_auteurs` (`jeu_id`, `a_id`) VALUES
-(1, 2),
-(2, 6),
-(3, 1),
-(4, 9),
-(5, 10),
-(6, 3),
-(7, 4),
-(8, 7),
-(9, 5),
-(10, 8),
-(11, 8);
 
 -- --------------------------------------------------------
 
@@ -262,23 +213,6 @@ CREATE TABLE `jeu_langues` (
   `l_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `jeu_langues`
---
-
-INSERT INTO `jeu_langues` (`jeu_id`, `l_id`) VALUES
-(1, 1),
-(2, 1),
-(3, 3),
-(4, 2),
-(5, 2),
-(6, 2),
-(7, 3),
-(8, 2),
-(9, 1),
-(10, 3),
-(11, 2);
-
 -- --------------------------------------------------------
 
 --
@@ -289,37 +223,6 @@ CREATE TABLE `jeu_theme` (
   `jeu_id` int(11) NOT NULL,
   `tdj_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `jeu_theme`
---
-
-INSERT INTO `jeu_theme` (`jeu_id`, `tdj_id`) VALUES
-(1, 2),
-(1, 12),
-(2, 3),
-(2, 7),
-(3, 6),
-(3, 10),
-(4, 3),
-(4, 8),
-(4, 9),
-(5, 5),
-(5, 12),
-(6, 3),
-(6, 9),
-(7, 3),
-(7, 14),
-(8, 2),
-(8, 9),
-(8, 12),
-(9, 3),
-(9, 7),
-(9, 14),
-(10, 6),
-(10, 10),
-(10, 15),
-(11, 8);
 
 -- --------------------------------------------------------
 
@@ -402,7 +305,9 @@ CREATE TABLE `pays` (
 INSERT INTO `pays` (`pays_id`, `pays_nom`) VALUES
 (1, 'France'),
 (2, 'Angleterre'),
-(3, 'Allemagne');
+(3, 'Allemagne'),
+(4, 'Japon'),
+(5, 'USA');
 
 -- --------------------------------------------------------
 
@@ -488,7 +393,7 @@ CREATE TABLE `utilisateurs` (
   `u_id` int(11) NOT NULL,
   `u_pseudo` varchar(50) NOT NULL,
   `u_email` varchar(50) NOT NULL,
-  `u_mdp` varchar(50) NOT NULL,
+  `u_mdp` varchar(255) DEFAULT NULL,
   `tu_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -675,7 +580,7 @@ ALTER TABLE `editeur`
 -- AUTO_INCREMENT pour la table `jeu`
 --
 ALTER TABLE `jeu`
-  MODIFY `jeu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `jeu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `langues`
@@ -699,7 +604,7 @@ ALTER TABLE `mecanisme`
 -- AUTO_INCREMENT pour la table `pays`
 --
 ALTER TABLE `pays`
-  MODIFY `pays_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `pays_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `theme_de_jeu`
--- script/script.js
+++ script/script.js
@@ -280,26 +280,26 @@ function afficheChamp(champ1, labelchamp1, select_param) {
   }
 }
 
-// Fonction pour gérer l'ajout au panier
+// // Fonction pour gérer l'ajout au panier
 
-let panier = [];
-let total = 0;
+// let panier = [];
+// let total = 0;
 
-function addToCart(nom, prix) {
-  // quantite = parseInt(quantite);
-  panier.push({ nom: nom, prix: prix });
-  total += prix;
-  showCart();
-}
+// function addToCart(nom, prix) {
+//   // quantite = parseInt(quantite);
+//   panier.push({ nom: nom, prix: prix });
+//   total += prix;
+//   showCart();
+// }
 
-//Fonction pour afficher le panier
-function showCart(){
-  const cartList = document.getElementById("cartList");
-  cartList.innerHTML = "";
-  panier.forEach((item) => {
-    const li = document.createElement("li");
-    li.textContent = `${item.nom}. Prix : ${item.prix}€.`;
-    cartList.appendChild(li);
-  });
-  document.getElementById("total").textContent = total;
-}
+// //Fonction pour afficher le panier
+// function showCart(){
+//   const cartList = document.getElementById("cartList");
+//   cartList.innerHTML = "";
+//   panier.forEach((item) => {
+//     const li = document.createElement("li");
+//     li.textContent = `${item.nom}. Prix : ${item.prix}€.`;
+//     cartList.appendChild(li);
+//   });
+//   document.getElementById("total").textContent = total;
+// }
